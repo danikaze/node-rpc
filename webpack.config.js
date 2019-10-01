ef3aafe98903b137955dbe7b08c8d3b05bb905c4
danikaze <danikaze@gmail.com>
2019-10-01 18:49:22 +0900

    First commit: TypeScript/webpack basic environment

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const { join } = require('path');
+const { DefinePlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const constants = require('./constants');
+
+module.exports = env => {
+  const isProd = env === 'production';
+
+  return {
+    mode: isProd ? 'production' : 'development',
+
+    devtool: isProd ? undefined : 'inline-source-map',
+
+    entry: {
+      app: ['src/index.ts'],
+    },
+
+    output: {
+      path: join(__dirname, 'app'),
+    },
+
+    watch: !isProd,
+
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|gif|jpeg|svg|woff|woff2|ttf|eot|ico)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash].[ext]',
+              outputPath: 'img',
+              publicPath: './img',
+            },
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json',
+            },
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new DefinePlugin({
+        ...(() => {
+          const c = { ...constants };
+          Object.keys(c).forEach(k => {
+            c[k] = JSON.stringify(c[k]);
+          });
+          return c;
+        })(),
+        NODE_ENV: JSON.stringify(isProd ? 'production' : 'development'),
+      }),
+    ].concat(isProd ? [new CleanWebpackPlugin()] : []),
+
+    target: 'node',
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+      plugins: [new TsconfigPathsPlugin({ configFile: 'tsconfig.json' })],
+    },
+
+    optimization: {
+      minimize: isProd,
+      namedModules: !isProd,
+    },
+
+    stats: {
+      children: false,
+    },
+  };
+};
