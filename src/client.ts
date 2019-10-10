da9c84f621a2268771bbd25c46967208d744ac98
danikaze <danikaze@gmail.com>
2019-10-10 21:10:52 +0900

    Accept filename as parameter in the client

--- src/client.ts
+++ src/client.ts
@@ -5,12 +5,23 @@ async function run(): Promise<void> {
   logEvent('CLIENT_START', { version: APP_VERSION });
 
   const client = new Client({
+    file: getClientFilename(),
     host: SERVER_HOST,
     port: SERVER_PORT,
-    file: 'draw',
   });
   await client.connect();
   await client.rpc();
+}
+
+function getClientFilename(): string {
+  const defaultFileName = 'draw';
+  const paramName = 'file';
+  const findStr = `--${paramName}=`;
+  const param = process.argv.filter(arg => arg.indexOf(findStr) !== -1)[0];
+  if (!param) {
+    return defaultFileName;
+  }
+  return param.substr(findStr.length);
 }
 
 run();
